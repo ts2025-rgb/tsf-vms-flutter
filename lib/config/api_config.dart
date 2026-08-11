1d0c2bd524b3f91ca4696c5b128e47694bedac36
ts2025-rgb <no.reply.tsf.backend@gmail.com>
2026-08-11 20:40:29 +0530

    Update api_config.dart

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -10,7 +10,7 @@ class ApiConfig {
   static const String _debugBaseUrl =
       "https://shrew-concrete-cobra.ngrok-free.app";
   static const String _productionBaseUrl =
-      "https://tsf-backend-production.up.railway.app";
+      "api.py4p.org";
 
   // Current base URL based on debug flag
   static String get baseUrl => kIsDebug ? _debugBaseUrl : _productionBaseUrl;
